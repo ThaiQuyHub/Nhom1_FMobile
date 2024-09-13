f1192181af803c21e190c47931878463afc25290
ThaiQuyHub <nhatquy10@gmail.com>
2024-09-13 13:38:15 +0700

    Source ngay 13 thang 09 (update 13h37)

---
+++ src/main/java/com/group1/FMobile/domain/dto/VerifyDTO.java
@@ -0,0 +1,13 @@
+package com.group1.fmobile.domain.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class VerifyDTO {
+    String otp;
+}
---
+++ src/main/webapp/WEB-INF/view/client/homepage/index.jsp
@@ -0,0 +1,3 @@
+<jsp:include page="header.jsp"/>
+<h1>Hieu dep trai</h1>
+<jsp:include page="footer.jsp"/>
