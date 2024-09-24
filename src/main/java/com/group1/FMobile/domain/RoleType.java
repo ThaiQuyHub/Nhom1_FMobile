c6f3145b2599e3a94cbf36debff8b1f9b4f0f131
DuongHieu20793 <duonghieu20793@gmail.com>
2024-09-24 18:23:36 +0700

    Delete src/main/java/com/group1/FMobile directory
    Xóa thư mục FMobile do sai tên đặt package 6:20 ngày 24/09/2024

--- src/main/java/com/group1/FMobile/domain/RoleType.java
+++
@@ -1,8 +0,0 @@
-package com.group1.fmobile.domain;
-
-public enum RoleType {
-
-    GUEST,
-USER,
-    ADMIN,
-}
--- src/main/java/com/group1/FMobile/domain/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.group1.fmobile.domain;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
